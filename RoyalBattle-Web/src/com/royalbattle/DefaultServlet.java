70c9044aceccc8858cc1816a6abb5228b1a7a6ca
Reforged Dream <reforgeddream@gmail.com>
2018-01-18 23:36:23 +0700

    All files are added to git

---
+++ RoyalBattle-Web/src/com/royalbattle/DefaultServlet.java
@@ -0,0 +1,51 @@
+package com.royalbattle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class DefaultServlet
+ */
+@WebServlet("/DefaultServlet")
+public class DefaultServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DefaultServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    //@EJB
+    //TestBean testBean;
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Параметр
+        String parameter = request.getParameter("parameter");
+        
+		System.out.println("Executing doGet method from DefaultServlet");
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		//out.println(testBean.testThisBean());
+		
+		out.println("Served at: " + request.getContextPath() + " request.getContextPath()");
+		out.println("<br>");
+		out.println("Параметр: " + parameter);
+		
+	}
+
+}
